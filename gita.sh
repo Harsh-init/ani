2c2b554d57c5210ef65acd2034aadd6d88dd56af
gurpal2004 <paljai2468@gamil.com>
2021-04-24 10:30:12 +0530

    Nothing

--- gita.sh
+++ gita.sh
@@ -1,5 +1,15 @@
 !/bin/sh
-echo '$1'
+
+if [ -n "$1" ]; then
+  echo "You supplied the first parameter!"
+  n1=$1
+else
+  echo "First parameter not supplied."
+  n1='Nothing'
+fi
+echo 'N1 = ------'
+echo $n1
+
 git add .
-git commit -m '$1'
+git commit -m  $n1
 git push
